70b2ae7a3198a579c517973677b34cc58eab58b0
audreyshotland <25ashotland@athenian.org>
2022-04-07 09:04:30 -0700

    day 1: name and math

--- src/print_input.py
+++ src/print_input.py
@@ -2,7 +2,16 @@
 # Collaborators: (enter people or resources who/that helped you)
 # If none, write none
 #
-#
+# Elena
 #############################
 
 # Write code here:
+print("hello world!")
+
+name = input("please state your name: ")
+print(f"hello {name}!")
+#print(name)
+
+num1 = 10
+num2 = 5
+print(num1/num2)
